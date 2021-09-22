dd79846ce2d963e707d8c533919d7e77f3c1bc2c
grandeto <grandeto.com@gmail.com>
2021-09-22 22:32:32 +0300

    get chat resources through ajax call

--- client/broadcast-channel.js
+++ client/broadcast-channel.js
@@ -9,79 +9,85 @@ var io = require("socket.io-client"),
     leaderElector = createLeaderElection(channel);
 
 var getCookie = function(cname) {
-    var name = cname + "=";
-    var ca = document.cookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-    var c = ca[i];
-    while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-    }
-    }
-    return "";
-},
-setCookie = function(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    var expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;Secure=true;SameSite=Lax";
-    }
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    },
+    setCookie = function(cname, cvalue, exdays) {
+        var d = new Date();
+        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+        var expires = "expires="+d.toUTCString();
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/;Secure=true;SameSite=Lax";
+    };
 
 channel.onmessage = msg => console.log('msg received', msg);
 
 leaderElector.awaitLeadership().then(function () {
-  console.log('is leader');
-  document.title = '♛ Is Leader!';
+    console.log('is leader');
+    document.title = '♛ Is Leader!';
 
-    var ioToken = function() {
-        return getCookie('chat_token');
-    };
+    var ioToken,
+        ioUserId,
+        ioServer,
+        xhr = new XMLHttpRequest(),
+        res;
 
-    var ioUserId = function() {
-        return getCookie('user_chat_uuid');
-    };
+    xhr.withCredentials = true;
+    xhr.addEventListener("readystatechange", function() {
+        if(this.readyState === 4) {
+            res = JSON.parse(this.responseText);
+            ioToken = res.auth_token;
+            ioUserId = res.chat_uuid;
+            ioServer = res.chat_server;
 
-    var ioServer = function() {
-        return getCookie('chat_server');
-    }
+            if (ioToken && ioUserId && ioServer) {
+                var socket = io(ioServer, {
+                    upgrade: true,
+                    rememberUpgrade: true,
+                    secure: true,
+                    rejectUnauthorized: false, // ssl verify
+                    reconnectionDelay: 5000,
+                    reconnectionDelayMax: 10000,
+                    auth: {
+                        token: ioToken
+                    }
+                });
 
-    if (ioToken() != "" && ioUserId() != "" && ioServer() != "") {
-        var socket = io(ioServer(), {
-            upgrade: true,
-            rememberUpgrade: true,
-            secure: true,
-            rejectUnauthorized: false, // ssl verify
-            reconnectionDelay: 5000,
-            reconnectionDelayMax: 10000,
-            auth: {
-                token: ioToken()
+                socket.on(ioUserId, (...args) => {
+                    // do something with the event data
+                    console.log('event handled by leader', args[0]);
+                    channel.postMessage(args[0]);
+                });
+
+                socket.on("connect_error", (err) => {
+                    if (err.message === "invalid token") {
+                        console.error('Invalid token');
+                        var chat_reload = getCookie('chat_reload');
+                        if (chat_reload == "" || +chat_reload < 6) {
+                            setCookie('chat_reload', +chat_reload+1, 1);
+                            setTimeout(function () {
+                                location.reload();
+                            }, 5000);
+                        } else {
+                            setTimeout(function () {
+                                setCookie('chat_reload', "", -1);
+                                location.reload();
+                            }, 600000);
+                        }
+                    }
+                });
             }
-        });
-
-        socket.on(ioUserId(), (...args) => {
-            // do something with the event data
-            console.log('event handled by leader', args[0]);
-            channel.postMessage(args[0]);
-        });
-
-        socket.on("connect_error", (err) => {
-            if (err.message === "invalid token") {
-                console.error('Invalid token');
-                var chat_reload = getCookie('chat_reload');
-                if (chat_reload == "" || +chat_reload < 6) {
-                    setCookie('chat_reload', +chat_reload+1, 1);
-                    setTimeout(function () {
-                        location.reload();
-                    }, 5000);
-                } else {
-                    setTimeout(function () {
-                        setCookie('chat_reload', "", -1);
-                        location.reload();
-                    }, 600000);
-                }
-            }
-        });
-    }
+        }
+    });
+    xhr.open("GET", document.location.origin + "/vrchats/resources");
+    xhr.send();
 });
