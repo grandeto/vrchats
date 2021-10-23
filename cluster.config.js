cb2a35e5d52430b88540b91647c54382e82c8033
grandeto <grandeto.com@gmail.com>
2021-10-23 16:42:20 +0300

    update cluster and standalone pm2 configs

--- cluster.config.js
+++ cluster.config.js
@@ -2,8 +2,9 @@ module.exports = {
     apps: [{
         name: "vrchatsc",
         script: "./app.js",
-        instances: "-1",
+        instances: process.env.CLUSTER_INSTANCES,
         exec_mode: "cluster",
+        exp_backoff_restart_delay: 500,
         node_args: "--nouse-idle-notification --expose-gc --max-old-space-size=8192",
     }]
 }
